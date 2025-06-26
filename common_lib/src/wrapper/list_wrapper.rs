1821cfb2de012766f40741e04fb39d3199d6ee15
小xun <1593437442@qq.com>
2025-06-26 06:55:00 +0000

    update common_lib/src/wrapper/list_wrapper.rs. 优化list包装
    Signed-off-by: 小xun <1593437442@qq.com>

--- common_lib/src/wrapper/list_wrapper.rs
+++ common_lib/src/wrapper/list_wrapper.rs
@@ -2,107 +2,42 @@ use crate::enums::WrapperErr;
 use crate::wrapper::ResponseWrapper;
 
 // 带列表数据的包装
-#[derive(Debug,serde::Serialize, PartialEq, Eq, Hash)]
+#[derive(Debug, serde::Serialize, PartialEq, Eq, Hash)]
 pub struct ListWrapper<T> {
-    pub base: ResponseWrapper,
-    pub data: T,
+    #[serde(flatten)]
+    base: ResponseWrapper,
+    data: Option<Vec<T>>,
 }
+
 impl<T> ListWrapper<T> {
-    /// 创建新的列表包装
-    pub fn new(base: ResponseWrapper, data: Vec<T>) -> Self {
-        ListWrapper { base, data }
-    }
-
-    /// 创建成功响应并携带数据列表
-    pub fn success(data: Vec<T>) -> Self {
-        ListWrapper {
+    pub fn new() -> Self {
+        Self {
             base: ResponseWrapper::success_default(),
-            data,
+            data: None,
         }
     }
 
-    /// 创建空列表的成功响应
-    pub fn success_empty() -> Self {
-        ListWrapper {
+    pub fn success(data: T) -> Self {
+        ObjectWrapper {
             base: ResponseWrapper::success_default(),
-            data: Vec::new(),
+            data: Some(data),
         }
     }
-
-    /// 创建失败响应并携带数据列表
-    pub fn fail(data: Vec<T>) -> Self {
-        ListWrapper {
-            base: ResponseWrapper::fail_default(),
-            data,
-        }
+    pub fn set_fail(&mut self, msg: impl Into<String>) {
+        self.base = ResponseWrapper::new(WrapperErr::Fail.code(), msg.into());
+        self.data = None;
     }
 
-    /// 从错误枚举创建列表包装
-    pub fn from_err(err: WrapperErr, data: Vec<T>) -> Self {
-        ListWrapper {
-            base: ResponseWrapper::from(err),
-            data,
-        }
+    pub fn set_unknown_error(&mut self, msg: impl Into<String>) {
+        self.base = ResponseWrapper::new(WrapperErr::UnknownError.code(), msg.into());
+        self.data = None;
     }
 
-    /// 获取基础响应包装的引用
     pub fn get_base(&self) -> &ResponseWrapper {
         &self.base
     }
 
-    /// 获取基础响应包装的可变引用
-    pub fn get_base_mut(&mut self) -> &mut ResponseWrapper {
-        &mut self.base
-    }
-
-    /// 获取数据的引用
-    pub fn get_data(&self) -> &Vec<T> {
-        &self.data
-    }
-
-    /// 获取数据的可变引用
-    pub fn get_data_mut(&mut self) -> &mut Vec<T> {
-        &mut self.data
-    }
-
-    /// 设置失败状态（保留现有数据）
-    pub fn set_fail(&mut self, msg: impl Into<String>) {
-        self.base = ResponseWrapper::new(WrapperErr::Fail.code(), msg);
-    }
-
-    /// 设置未知错误状态（保留现有数据）
-    pub fn set_unknown_error(&mut self, msg: impl Into<String>) {
-        self.base = ResponseWrapper::new(WrapperErr::UnknownError.code(), msg);
-    }
-
-    /// 转换数据为新的列表包装
-    pub fn map<U, F>(self, f: F) -> ListWrapper<U>
-    where
-        F: FnMut(T) -> U,
-    {
-        ListWrapper {
-            base: self.base,
-            data: self.data.into_iter().map(f).collect(),
-        }
-    }
-
-    /// 添加元素到列表
-    pub fn push(&mut self, item: T) {
-        self.data.push(item);
+    pub fn data(&self) -> Option<&Vec<T>> {
+        self.data.as_ref()
     }
 }
-
-// 实现 From 转换以便于从单个元素创建列表
-impl<T> From<T> for ListWrapper<T> {
-    fn from(item: T) -> Self {
-        ListWrapper::success(vec![item])
-    }
-}
-
-// 实现 From 转换以便于从 Vec 创建列表
-impl<T> From<Vec<T>> for ListWrapper<T> {
-    fn from(data: Vec<T>) -> Self {
-        ListWrapper::success(data)
-    }
-}
-
