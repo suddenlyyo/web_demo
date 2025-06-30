09d76d5b3db6e4a63d1a411d653c50c9fd881eae
小xun <1593437442@qq.com>
2025-06-30 03:27:46 +0000

    update common_lib/src/lib.rs.
    Signed-off-by: 小xun <1593437442@qq.com>

--- common_lib/src/lib.rs
+++ common_lib/src/lib.rs
@@ -26,7 +26,7 @@ mod tests {
     #[test]
     fn response_wrapper_test() {
         //let response = ResponseWrapper::success_default();
-        let response = ResponseWrapper::from(WrapperErrEnum::from_code(WrapperErrEnum::Success.code()));
+        let response = ResponseWrapper::from(WrapperErrEnum::from_code(WrapperErrEnum::Success.code()).expect("fail code!")));
         let response_serialized = serde_json::to_string(&response).unwrap();
         println!("response_serialized = {}", response_serialized);
         assert_eq!(response.get_code(), WrapperErrEnum::Success.code());
