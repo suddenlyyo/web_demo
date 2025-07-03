5d24cd8b6b84a219c32481852658e2ed8deec791
小xun <1593437442@qq.com>
2025-07-03 00:41:15 +0000

    update common_lib/src/wrapper/object_wrapper.rs.
    Signed-off-by: 小xun <1593437442@qq.com>

--- common_lib/src/wrapper/object_wrapper.rs
+++ common_lib/src/wrapper/object_wrapper.rs
@@ -26,13 +26,13 @@ impl<T> ObjectWrapper<T> {
     }
     
     // 默认失败响应
-    pub fn fail_default() -> Self {
+    pub fn fail_default(&self) -> Self {
         self.base.fail_default();
         self.data = None;
     }
 
     // 默认未知错误响应
-    pub fn unknown_error_default() -> Self {
+    pub fn unknown_error_default(&self) -> Self {
         self.base.unknown_error_default();
         self.data = None;
     }
