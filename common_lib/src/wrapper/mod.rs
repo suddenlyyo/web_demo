a022320f274add5c2810d6908007150f0d72befa
小xun <1593437442@qq.com>
2025-07-03 01:51:31 +0000

    update common_lib/src/wrapper/mod.rs.
    Signed-off-by: 小xun <1593437442@qq.com>

--- common_lib/src/wrapper/mod.rs
+++ common_lib/src/wrapper/mod.rs
@@ -4,4 +4,4 @@ mod object_wrapper;
 mod page_wrapper;
 mod response_wrapper;
 // 单行重新导出（按字母顺序）
-pub use {list_wrapper::*, object_wrapper::*, page_wrapper::*, response_wrapper::*};
+pub use {list_wrapper::*, object_wrapper::*, page_wrapper::*, response_wrapper::*,response_trait::*};
