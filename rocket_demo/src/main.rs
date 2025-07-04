5758a084e28b1dd0d498f72549507bd86d396c7e
小xun <1593437442@qq.com>
2025-07-04 07:30:24 +0000

    update rocket_demo/src/main.rs.
    Signed-off-by: 小xun <1593437442@qq.com>

--- rocket_demo/src/main.rs
+++ rocket_demo/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+//#[macro_use] extern crate rocket; //Rust 2015 宏导入语法
+use rocket::{get, routes};//Rust 2018及以上
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
