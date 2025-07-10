ed664911b7e08ca92d7e8a6b4a56abfcc1aba0d6
小xun <1593437442@qq.com>
2025-07-10 07:36:19 +0000

    update common_validation/src/enums/validation_rules.rs. 格式化处理
    Signed-off-by: 小xun <1593437442@qq.com>

--- common_validation/src/enums/validation_rules.rs
+++ common_validation/src/enums/validation_rules.rs
@@ -1,22 +1,22 @@
 // 验证规则枚举
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ValidationRulesEnum {
-    ///验证不允许为空
+    /// 验证不允许为空
     NotNone,
-    ///验证指定长度
+    /// 验证指定长度
     Length,
-    ///如果存在则验证长度，否则不验证
+    /// 如果存在则验证长度，否则不验证
     ExistLength,
-    ///验证指定格式的Date
+    /// 验证指定格式的Date
     Date,
-    ///验证指定格式的Time
+    /// 验证指定格式的Time
     Time,
-    ///验证指定格式的DateTime
+    /// 验证指定格式的DateTime
     DateTime,
-    ///验证数字的最小值
+    /// 验证数字的最小值
     NumberMin,
-    ///验证数字的最最值
+    /// 验证数字的最最值
     NumberMax,
-    ///验证整个结构体
+    /// 验证整个结构体
     Structure,
 }
