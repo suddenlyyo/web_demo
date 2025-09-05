545c5da429712f6df8d69344074eb2c129b9f85b
小xun <1593437442@qq.com>
2025-09-05 09:37:53 +0800

    去掉重复代码

--- rocket_demo/src/services/mod.rs
+++ rocket_demo/src/services/mod.rs
@@ -5,11 +5,3 @@ pub mod menu;
 pub mod role;
 pub mod user;
 
-/// 公共分页参数
-#[derive(Debug, Clone)]
-pub struct PageParam {
-    /// 页码
-    pub page_num: Option<u64>,
-    /// 每页条数
-    pub page_size: Option<u64>,
-}
